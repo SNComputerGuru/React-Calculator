3065ffd84278f16440e4339c08603bc224476e80
SNComputerGuru <shillingndivho4@gmail.com>
2025-04-19 03:13:44 +0200

    add test case for Multiply and add multiply function to calculator.js

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,6 +1,6 @@
 // src/calculator.test.js
 
-const { add, subtract } = require('./calculator');
+const { add, subtract , multiply} = require('./calculator');
 
 test('adds two numbers', () => {
   expect(add(3, 4)).toBe(7);
@@ -12,4 +12,10 @@ test('subtracts two numbers', () => {
   expect(subtract(10, 4)).toBe(6);
   expect(subtract(3, 7)).toBe(-4);
   expect(subtract(0, 0)).toBe(0);
+});
+
+test('multiplies two numbers', () => {
+  expect(multiply(10, 4)).toBe(40);
+  expect(multiply(3, 7)).toBe(21);
+  expect(multiply(0, 0)).toBe(0);
 });
